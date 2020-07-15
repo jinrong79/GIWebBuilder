5f14698d2f4291e4f78981951bcdc0e41ddef076
jinrong79 <rong.king@foxmail.com>
2020-07-15 19:33:18 +0800

    home-amd3900x

--- js/app/j79.app.js
+++ js/app/j79.app.js
@@ -17,10 +17,13 @@ class PageManager{
     }//-/
 
     getTopNaviHtml(){
-        let htmlStr=`<li><a href="${ this.naviURL.home}" target="_top">首页</a></li>
+    /*
+    <li><a href="${ this.naviURL.home}" target="_top">首页</a></li>
+    <li><a href="${ this.naviURL.privilege}"  target="_top">权限</a></li>
+        <li><a href="${ this.naviURL.role}"  target="_top">角色</a></li>*/
+        let htmlStr=`
             <li><a href="${ this.naviURL.user}"  target="_top">用户</a></li>
-            <li><a href="${ this.naviURL.privilege}"  target="_top">权限</a></li>
-            <li><a href="${ this.naviURL.role}"  target="_top">角色</a></li>
+           
             <li><a href="${ this.naviURL.log}"  target="_top">日志</a></li>
             <li><a href="${ this.naviURL.logout}"  target="_top"><i class="glyphicon glyphicon-off"></i> 退出</a></li>`;
 
